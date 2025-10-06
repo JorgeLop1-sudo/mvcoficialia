6be3c5e529b251e819bc368856314a9b41142a7c
JorgeLop1-sudo <rodriguezlopezjorgejulian@gmail.com>
2025-10-06 16:01:21 -0600

    add: Styles

--- partials/titulo.php
+++ partials/titulo.php
@@ -1,6 +1,6 @@
 <div class="header">
         <h1>Oficialía de Partes</h1>
-        <p>Sistema de Gestión de Trámites y Oficios</p>
+        <p>Sistema de Gestión de Oficios</p>
     </div>
     
     <div class="nav-links">
