e7c3035b0c06c735acd5c25dac7346b9d1012eeb
JorgeLop1-sudo <rodriguezlopezjorgejulian@gmail.com>
2025-10-07 12:44:59 -0600

    add: Styles

--- views/registrar.php
+++ views/registrar.php
@@ -174,7 +174,6 @@ header("Expires: 0");
     </script>
     <script src="../mvc_oficialiapartes/scripts/navbar.js"></script>
     <script src="../mvc_oficialiapartes/scripts/dark_theme_dash.js"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
